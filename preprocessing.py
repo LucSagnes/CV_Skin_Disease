9583bfc28c7269f206cbb9e9e27431068a2c33c8
Florian <florian.irrien@gmail.com>
2025-04-28 12:08:00 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -73,7 +73,6 @@ def generate_dataloader(
         shuffle: bool
 ) -> DataLoader:
     """
-    
     Parameters
     ----------
     dataset : Dataset
@@ -98,6 +97,28 @@ def generate_dataloader(
     Returns
     -------
     DataLoader
+
+    Example
+    -------
+    >>> dataset = load_dataset('marmal88/skin_cancer')
+    >>> preprocess_transform = transforms.Resize(size=(256, 256))
+    >>> label_encoder = LabelEncoder()
+    >>> minority_classes = [1, 4, 5]
+    >>> train_transform = transforms.Compose([
+    >>>     transforms.RandomCrop(size=(224, 224)),
+    >>>     transforms.RandomHorizontalFlip(p=0.5)
+    >>> ])
+    >>> generate_dataloader(
+    >>>     dataset=dataset,
+    >>>     part_set='train',
+    >>>     preprocess_transform=preprocess_transform,
+    >>>     label_encoder=label_encoder
+    >>>     minority_classes=minority_classes,
+    >>>     transform=train_transform,
+    >>>     train=True,
+    >>>     batch_size=32,
+    >>>     shuffle=True
+    >>> )
     """
     dataset = load_dataset(dataset)[part_set]
 
@@ -132,7 +153,7 @@ def generate_dataloader(
 
 def import_and_preprocess_image(
         dataset: datasets.Dataset,
-        preprocess_transform = None
+        preprocess_transform: transforms
     ) -> Tuple[torch.Tensor]:
     """
     Transform and preprocess the data.
@@ -141,7 +162,7 @@ def import_and_preprocess_image(
     ----------
     dataset : datasets.Dataset
         Dataset with data and label as a TensorDataset object
-    transformations : list of Transform objects
+    transformations : list of transforms objects
         List of transformations to be applied to the dataset
 
     Returns
